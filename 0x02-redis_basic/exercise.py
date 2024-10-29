6fffb1cfccf257ad8710577b987adc4b81661222
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-29 13:53:24 +0300

    Writing strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
-""" Hello"""
+""" Redis module"""
+
+import redis
+import uuid
 
 
+class Cache:
+    """Cache redis class"""
+
+    def __init__(self):
+        """Initializes"""
+
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """defines the store method"""
+
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+
+        return key
